542c39970e1794d2335847cace3d9fdcbe821bf4
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-11 10:41:42 +0300

    Lecture-6
    Talking about variables

---
+++ Section-2/Lecture-5.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var variableName = "Mammoth Interactive"
+
+var someInt = -8
+
+var someDouble = -2.3
+
+var someFloat:Double = 5.33432
+
+var someCGFloat: CGFloat = 0.34234
+
+var someBool = true
