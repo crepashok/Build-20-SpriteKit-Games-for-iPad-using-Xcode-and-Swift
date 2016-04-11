40b02aa7eb13d79a0bbce51722bd65320e509d0d
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-11 10:52:14 +0300

    Lecture-7
    Adding variables and talking about converting variables

--- Section-2/Lecture-5.playground/Contents.swift
+++ Section-2/Lecture-5.playground/Contents.swift
@@ -2,14 +2,19 @@
 
 import UIKit
 
-var variableName = "Mammoth Interactive"
+var someString: String = "Mammoth Interactive"
 
 var someInt = -8
 
-var someDouble = -2.3
+var someDouble:Double = -2.3
 
-var someFloat:Double = 5.33432
+var someFloat:Float = 5.33432
 
 var someCGFloat: CGFloat = 0.34234
 
 var someBool = true
+
+someDouble + Double(someFloat)
+
+someFloat + Float(someCGFloat)
+
