0dc74e4f7e1cbb904920e225df90824fc3c707e2
Pavlo Cretsu <c.pavlo90@gmail.com>
2016-04-11 10:58:29 +0300

    Lecture-8
    Introduction to arrays

--- Section-2/Lecture-5.playground/Contents.swift
+++ Section-2/Lecture-5.playground/Contents.swift
@@ -14,7 +14,14 @@ var someCGFloat: CGFloat = 0.34234
 
 var someBool = true
 
+var someStringArray = ["John", "Mark", "Lucy", "Sally", "Ian"]
+
+var someIntArray = [0, 1, 2, 3, 4, 5, 6]
+
 someDouble + Double(someFloat)
 
 someFloat + Float(someCGFloat)
 
+someStringArray[4]
+
+someIntArray[1]
